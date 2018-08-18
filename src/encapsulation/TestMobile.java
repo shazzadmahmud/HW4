8158574429a86a1e4b7ce9111f247c23d824ee16
Ali-Pathan <amp.rajive@gmail.com>
2018-08-18 13:47:35 -0400

    first git upload

--- src/encapsulation/TestMobile.java
+++ src/encapsulation/TestMobile.java
@@ -4,12 +4,12 @@ public class TestMobile {
 	 
     public static void main(String[] args) {
         Mobile mb = new Mobile();
-        mb.setMobilBrnd("iphone");
-        mb.getMobilBrnd();
-        mb.setMobileColor("White");
-        mb.getMobileColor();
+        mb.setMobilBrnd("Iphone");
+        mb.setMobileFeatures("Ratina Display");
+        mb.setMobileColor("Gold");
+        mb.setMobileModel(6);
          
-        System.out.println( mb.getMobilBrnd()+ " "+  mb.getMobileColor());
+        System.out.println( mb.getMobilBrnd()+ " "+ mb.getMobileFeatures()+ " " + mb.getMobileColor() + " " + mb.getMobileModel());
                     
  
     }
